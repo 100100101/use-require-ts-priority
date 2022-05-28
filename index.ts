e0c6330228083a03d60485529dd2defd1e0a1368
JSerGit <bibik.serej@gmail.com>
2022-05-28 15:05:13 +0300

    initial

---
+++ index.ts
@@ -0,0 +1,33 @@
+import fs from 'node:fs'
+const Module = require('node:module')
+// declare const Module: any
+export default () => {
+    const originalRequire = Module.prototype.require
+    Module.prototype.require = function (name) {
+        let isRequireTsFile = !!name.match(/.*\.ts$/)
+        const isRequireJsFile = !!name.match(/.*\.js$/)
+        if (isRequireJsFile) {
+            const fullPathRequiredJsFile = Module._resolveFilename(name, this)
+            const isJsFileExists = fs.existsSync(fullPathRequiredJsFile)
+            if (isJsFileExists) {
+                const replaceEntry = [/(.*)\.js$/, '$1.ts']
+                const fullPathRequiredTsFile = fullPathRequiredJsFile.replace(
+                    ...replaceEntry
+                )
+                const isTsFileExists = fs.existsSync(fullPathRequiredTsFile)
+                if (isTsFileExists) {
+                    isRequireTsFile = true
+                    name = name.replace(...replaceEntry)
+                }
+            }
+        }
+        const requiredModule = originalRequire.call(this, name)
+        // console.log('requiredModule:', name)
+
+        if (isRequireTsFile) {
+            return requiredModule.default
+        }
+
+        return requiredModule
+    }
+}
